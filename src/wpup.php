bd9fb46c19f4b7d32cb83bb7f2bb915f28c15252
Hokkaweb - Css <sametaydemir@yandex.com>
2015-05-31 01:52:31 +0300

    first commit

---
+++ src/wpup.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * Wordpress Installer
+ * @author Samet Aydemir <sametaydemir@yandex.com>
+ */
+
+namespace wpup;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use GuzzleHttp\ClientInterface;
+use ZipArchive;
+
+class wpup extends Command{
+
+    private $client;
+
+    public function __construct(ClientInterface $client){
+        $this->client = $client;
+        parent::__construct();
+
+    }
+
+
+    public function configure(){
+
+        $this->setName('new')
+             ->setDescription('Create a new Wordpress blog.')
+             ->addArgument('name', InputArgument::REQUIRED);
+    }
+
+
+    public function execute(InputInterface $input, OutputInterface $output){
+
+        $dir = getcwd() . '/' . $input->getArgument('name');
+
+        $output->writeln('<info>Downloading WordPress...</info>');
+
+        $this->notExist($dir,$output);
+
+        $this->downloadZipFile($zip = $this->fileName())
+             ->extractZipFile($zip, $dir)
+             ->cleanZipFile($zip);
+
+        $output->writeln('<comment>Wordpress blog ready!</comment>');
+
+    }
+
+
+    private function notExist($dir, OutputInterface $output){
+
+        if(is_dir($dir)){
+
+            $output->writeln('<error>Application already exists!</error>');
+            exit(1);
+
+        }
+
+    }
+
+
+    private function fileName(){
+        return getcwd() . '/wpup_' . md5(time().uniqid()) . '.zip';
+    }
+
+
+    private function downloadZipFile($zip){
+        // https://github.com/WordPress/WordPress/archive/master.zip
+        $request = $this->client->get('https://github.com/WordPress/WordPress/archive/master.zip')->getBody();
+        file_put_contents($zip,$request);
+        return $this;
+    }
+
+
+    private function extractZipFile($zip, $dir){
+
+        $arch = new ZipArchive;
+
+        $arch->open($zip);
+        $arch->extractTo($dir);
+        $arch->close();
+
+        return $this;
+    }
+
+
+    private function cleanZipFile($zip){
+        @chmod($zip,0777);
+        @unlink($zip);
+
+        return $this;
+    }
+
+}
